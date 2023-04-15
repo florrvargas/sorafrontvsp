99b5ae3719da5dc18f8eeb990067b938188b6e11
florrvargas <vflorencia33@gmail.com>
2023-04-15 02:59:50 -0300

    calculo viaje

--- src/Componentes/Perfil/SideBar/Viajes/Viajes.jsx
+++ src/Componentes/Perfil/SideBar/Viajes/Viajes.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Viajes.css'
 import SideBar from '../SideBar';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { useEffect } from 'react';
 
 
-const center = {lat:-32.9451582, lng: -60.6864795}
 
 export default function Viajes() {
 
@@ -12,6 +12,31 @@ export default function Viajes() {
     googleMapsApiKey: 'AIzaSyA_L3GuLKlfEebK6qSzJRRhs8g9vf3k7rI'
   })
 
+  const [latitud, setLatitud] = useState('');
+  const [longitud, setLongitud] = useState('');
+  const [timestamp, setTimestamp] = useState('');
+  const [done, setDone] = useState(false);
+  const [costo, setCosto] =useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      setLatitud(position.coords.latitude);
+      setLongitud(position.coords.longitude);
+      setTimestamp(position.timestamp)
+    })
+  })
+  
+  const center = {lat:latitud, lng: longitud}
+  const km = 2;
+  const tiempoAprox = 20;
+  
+  function result(){
+    const tarifaInicial = 100;
+    const costo = tarifaInicial * km;
+    setCosto(costo);
+    setDone(true);
+  } 
+  
   if(!isLoaded) return <div>Loading...</div>
 
   return (
@@ -34,9 +59,19 @@ export default function Viajes() {
       <p>Ej: direccion 1</p>
       <input placeholder="Destino" class="input" type="text"/>
       <p>Ej: direccion 2</p>
-      <button class="sigin-btn">Calcular</button>
- 
+      <button class="sigin-btn" onClick={result}>Calcular</button>
+      { done ? 
+        <div className="result">
+          <p>TIEMPO APROXIMADO: {tiempoAprox} min</p>
+          <div className="costo">
+            <h2>COSTO: </h2>
+            <h2 id='costo'>$ {costo}</h2>
+         </div> 
+        </div> : null
+      }
+
     
+ 
     </div>
     </div>
     
